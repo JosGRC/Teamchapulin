1e9c07d9969e03e91b18bd7e3e662754fb0f4a97
JosGRC <2109069@upy.edu.mx>
2022-04-04 18:34:32 -0500

    function min

--- main.c
+++ main.c
@@ -1,29 +1,11 @@
-#include "./max.h"
 #include "./min.h"
-#include "./odd.h"
-#include "./fin.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "./find.h"
 
-int arr[] = {3,4,6,57,5,345,45,6};
-int len = sizeof(arr)/sizeof(arr[0]);    
-int look = 345;
+int arr[5] = {8,4,2,9,1};
 
 int main(){
-	system("clear");
-	printf("\n\n\t***Number of elements present in given array: %d***\n\n", len);     
-
-	int maxIndex = max(arr, len);
-	printf("Max number Value: %d\t\t| \tMax number Index: %d\n", arr[maxIndex], maxIndex);
-
-	int minIndex = min(arr, len);
-	printf("Min number Value: %d\t\t| \tMin number Index: %d\n", arr[minIndex], minIndex);
-	
-	int oddCounter = odd(arr, len);
-	printf("Amount of Odd numbers: %d\n\n", oddCounter);
-	
-	int finIndex = fin(arr, len, look);
-	printf("Fin number Value: %d\t\t| \tFin number Index: %d\n", arr[finIndex], finIndex);
-	
-	return 0;
+    int minIndex = min(arr, 5);
+printf("minIndex  %d, value: %d\n", minIndex, arr[minIndex]);
+    return 0;
 }
