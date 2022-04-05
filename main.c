757951d493a48edb0ed1fc843508a70c2a12c368
JosGRC <2109069@upy.edu.mx>
2022-04-04 19:36:42 -0500

    fixed all

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
+#include "./max.h"
 #include "./min.h"
-#include <stdio.h>
+#include "./odd.h"
 #include "./find.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int arr[5] = {8,4,2,9,1};
+int arr[] = {34,65,32,65,9,3};
+int len = sizeof(arr)/sizeof(arr[0]);    
+int look = 65;
 
 int main(){
-    int minIndex = min(arr, 5);
-printf("minIndex  %d, value: %d\n", minIndex, arr[minIndex]);
-    return 0;
+	system("clear");
+	printf("\n\n\t***Number of elements present in given array: %d***\n\n", len);     
+
+	int maxIndex = max(arr, len);
+	printf("Max number Value: %d\t\t| \tMax number Index: %d\n", arr[maxIndex], maxIndex);
+
+	int minIndex = min(arr, len);
+	printf("Min number Value: %d\t\t| \tMin number Index: %d\n", arr[minIndex], minIndex);
+	
+	int oddCounter = odd(arr, len);
+	printf("Amount of Odd numbers: %d\n\n", oddCounter);
+	
+	int finIndex = find(arr, len, look);
+	printf("Fin number Value: %d\t\t| \tFin number Index: %d\n", arr[finIndex], finIndex);
+	
+	return 0;
 }
